d69168fa09fe7c53468ead2631d623a1f4a05be9
Hershi Shor <e0548597918@gmail.com>
2025-06-09 15:13:07 +0300

    new model reports class for intelReports table

---
+++ Models/IntelReport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon
+{
+    internal class IntelReport
+    {
+        public int ReporterId { get; set; }
+        public int TargetId { get; set; }
+        public string Text { get; set; }
+
+        public IntelReport(int reportId, int targetId, string text)
+        {
+            this.ReporterId = reportId;
+            this.TargetId = targetId;
+            this.Text = text;
+        }
+
+    }
+}
