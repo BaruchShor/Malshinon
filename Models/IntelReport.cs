64b9e1c2b059eaea6faf25e3f7bfb3dadd7b2cb4
Hershi Shor <e0548597918@gmail.com>
2025-06-10 10:14:13 +0300

    new constractor to get reports

--- Models/IntelReport.cs
+++ Models/IntelReport.cs
@@ -8,15 +8,28 @@ namespace Malshinon
 {
     internal class IntelReport
     {
+        public int NumMention { get; set; }
+        public int NumReports { get; set; }
         public int ReporterId { get; set; }
         public int TargetId { get; set; }
         public string Text { get; set; }
+        public DateTime DateTime { get; set; }
 
         public IntelReport(int reportId, int targetId, string text)
         {
             this.ReporterId = reportId;
             this.TargetId = targetId;
             this.Text = text;
+        }
+
+        public IntelReport(int numMention, int numReports ,int reportId, int targetId, string text, DateTime dateTime)
+        {
+            this.NumMention = numMention;
+            this.NumReports = numReports;
+            this.ReporterId = reportId;
+            this.TargetId = targetId;
+            this.Text = text;
+            this.DateTime = dateTime;
         }
 
     }
